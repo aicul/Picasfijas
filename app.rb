fe13d0e727a2770dcbb3be0f6394cd71c7dc17a1
kleer <kleer@kleer-csd-box>
2018-04-27 14:05:14 -0500

    Logica para niveles

--- app.rb
+++ app.rb
@@ -4,18 +4,22 @@ require './lib/Picas.rb'
 
 
 get '/' do
-	session["secreto"] = "murcielago"
-	session["numeros"] = ""
-
-	session["intentos"] = 10
 	
-
+	session["numeros"] = ""
+	session["intentos"] = 10	
 	session["picas"] = Picas.new	
 
 	erb(:index)
 end
 
 post '/iniciar' do	
+	if params["nivel"] == "Bajo"
+		session["secreto"] = "tres"
+	elsif params["nivel"] == "Medio"
+		session["secreto"] = "cuanto"
+	else
+		session["secreto"] = "formidable"
+	end
 	session["pista"] = session["picas"].dimensionar session["secreto"]
 	erb(:juego)
 end
@@ -23,9 +27,7 @@ end
 post '/validar' do
 	picas = Picas.new	
 	session["intentos"] = (session["picas"].intentos).to_i - 1
-
 	session["numeros"] +=  (session["picas"].validar session["secreto"], params["numero"]).to_s	
-
 	erb(:juego)
 end
 
